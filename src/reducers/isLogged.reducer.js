b2b3b012187407a04454792a2005099ac164660a
officialtpss <officialtpss@gmail.com>
2019-02-06 07:31:30 +0530

    clean

--- src/reducers/isLogged.reducer.js
+++
@@ -1,6 +0,0 @@
-import StorageService from './../storage/StorageService';
-export function isLogged(state = {}) {
-    return {
-         logedIn: (StorageService.getLogin()===null)? false: true
-      };    
-}
